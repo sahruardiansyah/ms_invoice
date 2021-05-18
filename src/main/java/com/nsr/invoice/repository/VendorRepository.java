f11fafe9213c07069644e94b87fa959c7d14aaf3
DESKTOP-I58SA2H\Andreas Daniel S <sahru.ardiansyah@cinema21.co.id>
2021-05-18 17:00:04 +0700

    fix response

--- src/main/java/com/nsr/invoice/repository/VendorRepository.java
+++ src/main/java/com/nsr/invoice/repository/VendorRepository.java
@@ -1,10 +1,13 @@
 package com.nsr.invoice.repository;
 
 import com.nsr.invoice.entity.Vendor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
+
 
 }
